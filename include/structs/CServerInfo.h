a4b4485c5cef2abe086faa4561e7f1e1f6f8ef09
jmd <jmd2028@gmail.com>
2013-06-21 04:53:10 -0700

    include/structs: Add CServerInfo.h

---
+++ include/structs/CServerInfo.h
@@ -0,0 +1,82 @@
+#ifndef _NX_NWN_STRUCT_CSERVERINFO_
+#define _NX_NWN_STRUCT_CSERVERINFO_
+
+struct CServerInfo_s {
+   uint32_t    field_00;                /* 0000 */
+   uint32_t    field_04;                /* 0004 */
+   uint32_t    field_08;                /* 0008 */
+   uint32_t    field_0C;                /* 000C */
+   uint32_t    field_10;                /* 0010 */
+   uint32_t    field_14;                /* 0014 */
+   uint32_t    field_18;                /* 0018 */
+   uint32_t    field_1C;                /* 001C */
+   uint32_t    field_20;                /* 0020 */
+   uint32_t    field_24;                /* 0024 */
+   uint32_t    field_28;                /* 0028 */
+   uint32_t    field_2C;                /* 002C */
+   uint32_t    field_30;                /* 0030 */
+   uint32_t    field_34;                /* 0034 */
+   uint32_t    field_38;                /* 0038 */
+   uint32_t    field_3C;                /* 003C */
+   uint32_t    field_40;                /* 0040 */
+   uint32_t    field_44;                /* 0044 */
+   uint32_t    field_48;                /* 0048 */
+   uint32_t    field_4C;                /* 004C */
+   uint32_t    field_50;                /* 0050 */
+   uint32_t    field_54;                /* 0054 */
+   uint32_t    field_58;                /* 0058 */
+   uint32_t    field_5C;                /* 005C */
+   uint32_t    field_60;                /* 0060 */
+   uint32_t    field_64;                /* 0064 */
+   uint32_t    field_68;                /* 0068 */
+   uint32_t    field_6C;                /* 006C */
+   uint32_t    field_70;                /* 0070 */
+   uint32_t    field_74;                /* 0074 */
+   uint32_t    field_78;                /* 0078 */
+   uint32_t    field_7C;                /* 007C */
+   uint32_t    field_80;                /* 0080 */
+   uint32_t    field_84;                /* 0084 */
+   uint32_t    field_88;                /* 0088 */
+   uint32_t    field_8C;                /* 008C */
+   uint32_t    field_90;                /* 0090 */
+   uint32_t    field_94;                /* 0094 */
+   uint32_t    field_98;                /* 0098 */
+   uint32_t    field_9C;                /* 009C */
+   uint32_t    field_A0;                /* 00A0 */
+   uint32_t    field_A4;                /* 00A4 */
+   uint32_t    field_A8;                /* 00A8 */
+   uint32_t    field_AC;                /* 00AC */
+   uint32_t    sri_min_level;           /* 00B0 */
+   uint32_t    sri_max_level;           /* 00B4 */
+   uint32_t    field_B8;                /* 00B8 */
+   uint32_t    field_BC;                /* 00BC */
+   uint32_t    field_C0;                /* 00C0 */
+   uint32_t    field_C4;                /* 00C4 */
+   uint32_t    field_C8;                /* 00C8 */
+   uint32_t    field_CC;                /* 00CC */
+   uint32_t    field_D0;                /* 00D0 */
+   uint32_t    field_D4;                /* 00D4 */
+   uint32_t    field_D8;                /* 00D8 */
+   uint32_t    field_DC;                /* 00DC */
+   uint32_t    field_E0;                /* 00E0 */
+   uint32_t    field_E4;                /* 00E4 */
+   uint32_t    field_E8;                /* 00E8 */
+   uint32_t    field_EC;                /* 00EC */
+   uint32_t    field_F0;                /* 00F0 */
+   int32_t     sri_using_irl;           /* 00F4 */
+   uint32_t    field_F8;                /* 00F8 */
+   uint32_t    field_FC;                /* 00FC */
+   uint32_t    field_100;               /* 0100 */
+   uint32_t    field_104;               /* 0104 */
+   uint32_t    field_108;               /* 0108 */
+   uint32_t    field_10C;               /* 010C */
+   uint32_t    field_110;               /* 0110 */
+   uint32_t    field_114;               /* 0114 */
+   uint32_t    sri_max_hp;              /* 0118 */
+   uint32_t    field_11C;               /* 011C */
+   uint32_t    field_120;               /* 0120 */
+   uint32_t    sri_hide_hp;             /* 0124 */
+};
+
+#endif
+
