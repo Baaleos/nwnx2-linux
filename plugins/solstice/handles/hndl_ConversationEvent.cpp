ed6fc0e041f064e5181b084b102797cd35846648
jmd <jmd2028@gmail.com>
2013-07-22 09:15:36 -0700

    plugins/solstice: Fix debug level in Handle_ConversationEvent.

--- plugins/solstice/handles/hndl_ConversationEvent.cpp
+++ plugins/solstice/handles/hndl_ConversationEvent.cpp
@@ -4,7 +4,7 @@ extern CNWNXSolstice solstice;
 
 int Handle_ConversationEvent(WPARAM p, LPARAM a) {
     int32_t event = *(int32_t *)p;
-    solstice.Log(0, "Conversatio Type: %d\n", event);
+    solstice.Log(3, "Conversation Type: %d\n", event);
     solstice.in_conditional_script = event == 2;
     return 0;
 }
