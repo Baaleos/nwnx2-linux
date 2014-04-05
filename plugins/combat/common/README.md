ee34e2643879bf6e2e7b4bf8a1a8893c7ef77db3
jmd <jmd2028@gmail.com>
2014-04-05 05:43:09 -0700

    nwnx_combat is no more and has been merged into nwnx_solstice.

--- plugins/combat/common/README.md
+++
@@ -1,3 +0,0 @@
-The common directory contains functions that query NWN
-structures and are not dependent on the internal state
-of nwnx_combat.
--- plugins/combat/handles.h
+++
@@ -1,9 +0,0 @@
-#ifndef NWNX_COMBAT_HANDLES_H
-#define NWNX_COMBAT_HANDLES_H
-
-int Handle_EffectEvent(WPARAM p, LPARAM a);
-int Handle_Event(WPARAM p, LPARAM a);
-int Handle_ItemPropEvent(WPARAM p, LPARAM a);
-int Handle_PluginsLoaded(WPARAM p, LPARAM a);
-
-#endif // NWNX_COMBAT_HANDLES_H
